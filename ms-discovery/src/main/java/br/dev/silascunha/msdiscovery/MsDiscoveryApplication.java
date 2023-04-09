722b18cdd3ac36cd86f4e9b2f4aed5bdbe00e57d
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2023-04-08 23:36:36 -0300

    config inicial do ms-discovery com spring security

--- ms-discovery/src/main/java/br/dev/silascunha/msdiscovery/MsDiscoveryApplication.java
+++ ms-discovery/src/main/java/br/dev/silascunha/msdiscovery/MsDiscoveryApplication.java
@@ -2,8 +2,10 @@ package br.dev.silascunha.msdiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsDiscoveryApplication {
 
 	public static void main(String[] args) {
